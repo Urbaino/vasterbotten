cd007f9e6ce50dad2d20b2bab4d9b92074202375
Martin Larsson <martin.larsson.6@gmail.com>
2022-08-10 17:44:25 +0000

    Show your turn status in /status

--- src/dominions/replies/currentGames.ts
+++ src/dominions/replies/currentGames.ts
@@ -2,11 +2,11 @@ import { codeBlock } from "@discordjs/builders";
 import { InteractionReplyOptions } from "discord.js";
 import { Player, PretenderService } from "../../types/pretenderService";
 import Status from "../../types/status";
+import { TurnStatus } from "../../types/turnStatus";
 
-const gameStatusString = (game: Status) => game.gameName + (game.gameStarted() ? '' : ' (Ej startat)')
 
 const currentGames: (player: Player, service: PretenderService) => Promise<InteractionReplyOptions> = async (player, service) => {
-    let content = []
+    const content = []
     const currentGames = service.gameNames().map(name => service.status(name)).filter(g => g !== null) as Status[]
     if (!currentGames.length) {
         return {
@@ -16,17 +16,30 @@ const currentGames: (player: Player, service: PretenderService) => Promise<Inter
         };
     }
 
-    const playerGames = currentGames.filter(g => g.playerNation(player))
-    if (playerGames.length) {
-        content.push('Du spelar i följande spel:')
-        content.push(codeBlock(playerGames.map(gameStatusString).join('\n')))
+    const started: Status[] = []
+    const notStarted: Status[] = []
+    const notPlaying: Status[] = []
+
+    currentGames.forEach(game => {
+        if (!game.playerNation(player)) notPlaying.push(game)
+        else if (!game.gameStarted()) notStarted.push(game)
+        else started.push(game)
+    })
+
+    if (started.length) {
+        content.push('Du spelar i följande spel: (* indikerar din tur)')
+        content.push(codeBlock(started.map(g => (g.playerNation(player)?.turnStatus === TurnStatus.Finished ? '' : '*') + g.gameName).join('\n')))
     }
-    else {
+    if (notStarted.length) {
+        content.push('Dina ej startade spel:')
+        content.push(codeBlock(notStarted.map(g => g.gameName).join('\n')))
+    }
+    if (notPlaying.length === currentGames.length) {
         content.push('Du är inte med i något spel.')
     }
-    if (currentGames.length > playerGames.length) {
+    if (notPlaying.length) {
         content.push('Följande övriga spel finns:')
-        content.push(codeBlock(currentGames.filter(g => !g.playerNation(player)).map(gameStatusString).join('\n')))
+        content.push(codeBlock(notPlaying.map(g => g.gameName).join('\n')))
     }
 
     return {
